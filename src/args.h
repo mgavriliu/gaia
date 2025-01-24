149d924f254144d728cd50e8299c5dcb6f00e7fc
Marcel Gavriliu <mgavriliu@gmail.com>
2025-01-24 15:57:50 -0800

    Initial commit

---
+++ src/args.h
@@ -0,0 +1,20 @@
+
+#pragma once
+#include <string>
+#include <stdexcept>
+
+class Args {
+public:
+    static Args parse(int argc, char* argv[]) {
+        if (argc < 2) {
+            throw std::runtime_error("Usage: " + std::string(argv[0]) + " <json_file>");
+        }
+        return Args{ argv[1] };
+    }
+
+    const std::string& getJsonFile() const { return jsonFile; }
+
+private:
+    Args(const std::string& jsonFile) : jsonFile(jsonFile) {}
+    std::string jsonFile;
+};
